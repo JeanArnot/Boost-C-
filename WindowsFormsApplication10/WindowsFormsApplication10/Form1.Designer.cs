b2411ec70423c59f64050b6dde60cd0d862e987a
JeanArnot <ralaizanakejeanarnot@gmail.com>
2025-06-16 13:24:04 +0200

    Premier commit

---
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelAdresse = new System.Windows.Forms.Label();
+            this.labelPrenoms = new System.Windows.Forms.Label();
+            this.labelNom = new System.Windows.Forms.Label();
+            this.textBoxADRESSE = new System.Windows.Forms.TextBox();
+            this.textBoxPRENOM = new System.Windows.Forms.TextBox();
+            this.textBoxNOM = new System.Windows.Forms.TextBox();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SlateGray;
+            this.panel1.Controls.Add(this.labelId);
+            this.panel1.Controls.Add(this.labelAdresse);
+            this.panel1.Controls.Add(this.labelPrenoms);
+            this.panel1.Controls.Add(this.labelNom);
+            this.panel1.Controls.Add(this.textBoxADRESSE);
+            this.panel1.Controls.Add(this.textBoxPRENOM);
+            this.panel1.Controls.Add(this.textBoxNOM);
+            this.panel1.Controls.Add(this.textBoxID);
+            this.panel1.Location = new System.Drawing.Point(28, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(252, 245);
+            this.panel1.TabIndex = 0;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(12, 30);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(16, 13);
+            this.labelId.TabIndex = 9;
+            this.labelId.Text = "Id";
+            // 
+            // labelAdresse
+            // 
+            this.labelAdresse.AutoSize = true;
+            this.labelAdresse.Location = new System.Drawing.Point(9, 182);
+            this.labelAdresse.Name = "labelAdresse";
+            this.labelAdresse.Size = new System.Drawing.Size(45, 13);
+            this.labelAdresse.TabIndex = 7;
+            this.labelAdresse.Text = "Adresse";
+            // 
+            // labelPrenoms
+            // 
+            this.labelPrenoms.AutoSize = true;
+            this.labelPrenoms.Location = new System.Drawing.Point(8, 125);
+            this.labelPrenoms.Name = "labelPrenoms";
+            this.labelPrenoms.Size = new System.Drawing.Size(48, 13);
+            this.labelPrenoms.TabIndex = 6;
+            this.labelPrenoms.Text = "Prenoms";
+            // 
+            // labelNom
+            // 
+            this.labelNom.AutoSize = true;
+            this.labelNom.Location = new System.Drawing.Point(9, 74);
+            this.labelNom.Name = "labelNom";
+            this.labelNom.Size = new System.Drawing.Size(29, 13);
+            this.labelNom.TabIndex = 5;
+            this.labelNom.Text = "Nom";
+            // 
+            // textBoxADRESSE
+            // 
+            this.textBoxADRESSE.Location = new System.Drawing.Point(86, 182);
+            this.textBoxADRESSE.Name = "textBoxADRESSE";
+            this.textBoxADRESSE.Size = new System.Drawing.Size(151, 20);
+            this.textBoxADRESSE.TabIndex = 3;
+            // 
+            // textBoxPRENOM
+            // 
+            this.textBoxPRENOM.Location = new System.Drawing.Point(86, 125);
+            this.textBoxPRENOM.Name = "textBoxPRENOM";
+            this.textBoxPRENOM.Size = new System.Drawing.Size(151, 20);
+            this.textBoxPRENOM.TabIndex = 2;
+            // 
+            // textBoxNOM
+            // 
+            this.textBoxNOM.Location = new System.Drawing.Point(86, 81);
+            this.textBoxNOM.Name = "textBoxNOM";
+            this.textBoxNOM.Size = new System.Drawing.Size(151, 20);
+            this.textBoxNOM.TabIndex = 1;
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(86, 30);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(151, 20);
+            this.textBoxID.TabIndex = 0;
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.Location = new System.Drawing.Point(708, 20);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSupprimer.TabIndex = 12;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnModifier
+            // 
+            this.btnModifier.Location = new System.Drawing.Point(513, 20);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 11;
+            this.btnModifier.Text = "Modifier";
+            this.btnModifier.UseVisualStyleBackColor = true;
+            this.btnModifier.Click += new System.EventHandler(this.btnModifier_Click);
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.Location = new System.Drawing.Point(305, 20);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 10;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel2.Controls.Add(this.btnSupprimer);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.btnAjouter);
+            this.panel2.Controls.Add(this.btnModifier);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Location = new System.Drawing.Point(12, 47);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(975, 427);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(314, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(575, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(987, 41);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(417, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gestion Client";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 466);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnModifier;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label labelAdresse;
+        private System.Windows.Forms.Label labelPrenoms;
+        private System.Windows.Forms.Label labelNom;
+        private System.Windows.Forms.TextBox textBoxADRESSE;
+        private System.Windows.Forms.TextBox textBoxPRENOM;
+        private System.Windows.Forms.TextBox textBoxNOM;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication10/WindowsFormsApplication10/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication10
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnx;
+
+        public Form1()
+        {
+            cnx = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=c:\users\gasy\documents\visual studio 2012\Projects\WindowsFormsApplication10\WindowsFormsApplication10\Clients.mdf;Integrated Security=True;Connect Timeout=30");
+            InitializeComponent();
+            // Test connection
+            try
+            {
+                cnx.Open();
+                MessageBox.Show("Connection réussie !");
+                cnx.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur de connexion : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // Charge les données au démarrage
+            ChargerDonnees();
+        }
+
+        // Fonction pour charger les données dans le DataGridView
+        private void ChargerDonnees()
+        {
+            try
+            {
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+                cnx.Open();
+                string sql = "SELECT * FROM Clients";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cnx);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors du chargement des données : " + ex.Message + "\nStackTrace: " + ex.StackTrace, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+            }
+        }
+
+        // Fonction de validation des champs
+        private bool ValiderChamps()
+        {
+            if (string.IsNullOrEmpty(textBoxID.Text) || string.IsNullOrEmpty(textBoxNOM.Text) ||
+                string.IsNullOrEmpty(textBoxPRENOM.Text) || string.IsNullOrEmpty(textBoxADRESSE.Text))
+            {
+                MessageBox.Show("Veuillez remplir tous les champs !", "Avertissement", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+
+            if (!Regex.IsMatch(textBoxID.Text, @"^\d+$"))
+            {
+                MessageBox.Show("L'ID doit contenir uniquement des chiffres !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!Regex.IsMatch(textBoxNOM.Text, @"^[a-zA-Z\s]+$"))
+            {
+                MessageBox.Show("Le Nom doit contenir uniquement des lettres et des espaces !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!Regex.IsMatch(textBoxPRENOM.Text, @"^[a-zA-Z\s]+$"))
+            {
+                MessageBox.Show("Le Prénom doit contenir uniquement des lettres et des espaces !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!ValiderChamps())
+                    return;
+
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+                cnx.Open();
+                string sql = "INSERT INTO Clients (id, nom, prenoms, Adresse) VALUES (@id, @nom, @prenoms, @adresse)";
+                SqlCommand cmd = new SqlCommand(sql, cnx);
+                cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+                cmd.Parameters.AddWithValue("@nom", textBoxNOM.Text);
+                cmd.Parameters.AddWithValue("@prenoms", textBoxPRENOM.Text);
+                cmd.Parameters.AddWithValue("@adresse", textBoxADRESSE.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Client ajouté avec succès !", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ChargerDonnees();
+                textBoxID.Clear();
+                textBoxNOM.Clear();
+                textBoxPRENOM.Clear();
+                textBoxADRESSE.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de l'ajout : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+            }
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBoxID.Text))
+                {
+                    MessageBox.Show("Veuillez sélectionner un client à modifier !", "Avertissement", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!ValiderChamps())
+                    return;
+
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+                cnx.Open();
+                string sql = "UPDATE Clients SET nom = @nom, prenoms = @prenoms, Adresse = @adresse WHERE id = @id";
+                SqlCommand cmd = new SqlCommand(sql, cnx);
+                cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+                cmd.Parameters.AddWithValue("@nom", textBoxNOM.Text);
+                cmd.Parameters.AddWithValue("@prenoms", textBoxPRENOM.Text);
+                cmd.Parameters.AddWithValue("@adresse", textBoxADRESSE.Text);
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("Client modifié avec succès !", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    ChargerDonnees();
+                    textBoxID.Clear();
+                    textBoxNOM.Clear();
+                    textBoxPRENOM.Clear();
+                    textBoxADRESSE.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Aucun client trouvé avec cet ID.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la modification : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+            }
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(textBoxID.Text))
+                {
+                    MessageBox.Show("Veuillez sélectionner un client à supprimer !", "Avertissement", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                DialogResult result = MessageBox.Show("Voulez-vous vraiment supprimer ce client ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    if (cnx.State != ConnectionState.Closed)
+                        cnx.Close();
+                    cnx.Open();
+                    string sql = "DELETE FROM Clients WHERE id = @id";
+                    SqlCommand cmd = new SqlCommand(sql, cnx);
+                    cmd.Parameters.AddWithValue("@id", textBoxID.Text);
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Client supprimé avec succès !", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        ChargerDonnees();
+                        textBoxID.Clear();
+                        textBoxNOM.Clear();
+                        textBoxPRENOM.Clear();
+                        textBoxADRESSE.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Aucun client trouvé avec cet ID.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur lors de la suppression : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (cnx.State != ConnectionState.Closed)
+                    cnx.Close();
+            }
+        }
+
+        // Événement pour remplir les TextBox lors de la sélection d'une ligne dans le DataGridView
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Vider les TextBox avant de remplir
+            textBoxID.Clear();
+            textBoxNOM.Clear();
+            textBoxPRENOM.Clear();
+            textBoxADRESSE.Clear();
+
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+                textBoxID.Text = row.Cells["id"].Value != null ? row.Cells["id"].Value.ToString() : "";
+                textBoxNOM.Text = row.Cells["nom"].Value != null ? row.Cells["nom"].Value.ToString() : "";
+                textBoxPRENOM.Text = row.Cells["prenoms"].Value != null ? row.Cells["prenoms"].Value.ToString() : "";
+                textBoxADRESSE.Text = row.Cells["Adresse"].Value != null ? row.Cells["Adresse"].Value.ToString() : "";
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
